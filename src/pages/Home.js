3791ebd01ce19df9d53bfb15117cc9bcc27adab2
zwu3 <99296226+zwu3@users.noreply.github.com>
2023-11-21 16:45:07 -0800

    spotify logo login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { handleLogin } from '../service/spotifyService';
+import '../css/Home.css';
+import SpotifyLogo from '../logo/Spotify_icon.svg';
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Welcome to your Spotify data dashboard!</h1>
-      <button onClick={handleLogin}>Login with Spotify</button>
+      <button onClick={handleLogin} className="spotify-login-btn">
+        <img src={SpotifyLogo} alt="Spotify" className="spotify-logo" />
+        Login with Spotify
+      </button>
     </div>
   );
 };
