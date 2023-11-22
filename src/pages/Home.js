844fae9cff0059e07292b0c1787194f597e16c9f
zwu3 <99296226+zwu3@users.noreply.github.com>
2023-11-21 17:05:36 -0800

    spotify icon for login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { handleLogin } from '../service/spotifyService';
+import { useSpotifyAuth } from '../components/SpotifyAuthContext';
 import '../css/Home.css';
-import SpotifyLogo from '../logo/Spotify_icon.svg';
+import SpotifyLogo from '../logo/Spotify_icon.png'; // Make sure the path is correct
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { accessToken } = useSpotifyAuth();
+  console.log('token', accessToken);
+  const goToDashboard = () => {
+    navigate('/dashboard');
+  };
+
   return (
     <div className="home-container">
       <h1>Welcome to your Spotify data dashboard!</h1>
-      <button onClick={handleLogin} className="spotify-login-btn">
-        <img src={SpotifyLogo} alt="Spotify" className="spotify-logo" />
-        Login with Spotify
-      </button>
+      {!accessToken ? (
+        <button onClick={handleLogin} className="spotify-login-btn">
+          <img src={SpotifyLogo} alt="Spotify" className="spotify-logo" /> {/* Spotify logo */}
+          Login with Spotify
+        </button>
+      ) : (
+        <button onClick={goToDashboard} className="dashboard-btn">
+          Go to Dashboard
+        </button>
+      )}
     </div>
   );
 };
