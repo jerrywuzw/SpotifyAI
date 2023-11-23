82a8b79e13bb23645ed625f26972581eff4654c6
zwu3 <99296226+zwu3@users.noreply.github.com>
2023-11-23 09:12:41 -0800

    navbar update with styling

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Navbar.css'; 
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-brand">Spotify AI</div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-      </ul>
-    </nav>
-  );
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+    return (
+        <nav className="navbar">
+          <div className="hamburger-menu" onClick={toggleDropdown}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <div className="navbar-brand">Spotify AI</div>
+          <div className={`dropdown ${dropdownOpen ? 'dropdown-open' : ''}`}>
+            <ul className="nav-links">
+              <li><Link to="/" onClick={() => setDropdownOpen(false)}>Home</Link></li>
+              <li><Link to="/dashboard" onClick={() => setDropdownOpen(false)}>Dashboard</Link></li>
+              {/* other links */}
+            </ul>
+          </div>
+        </nav>
+    );
 };
 
 export default Navbar;
