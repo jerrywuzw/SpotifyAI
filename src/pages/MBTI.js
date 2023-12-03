3ba8e173f9c751ff4201b3e95afc35473c4ef462
zwu3 <99296226+zwu3@users.noreply.github.com>
2023-12-02 23:07:24 -0800

    MBTI updates

--- src/pages/MBTI.js
+++ src/pages/MBTI.js
@@ -1,37 +1,62 @@
 import React, { useState } from 'react';
 import '../css/MBTI.css';
 
-const MBTI = () => {
-  const [mbtiType, setMbtiType] = useState('');
-
-  const handleMbtiChange = (event) => {
-    setMbtiType(event.target.value);
-  };
-
-  return (
-    <div className="mbti-container">
-      <h1>Select Your MBTI Personality Type</h1>
-      <select value={mbtiType} onChange={handleMbtiChange} className="mbti-select">
-        <option value="">--Select Type--</option>
-        <option value="INTJ">INTJ - Architect</option>
-        <option value="INTP">INTP - Logician</option>
-        <option value="ENTJ">ENTJ - Commander</option>
-        <option value="ENTP">ENTP - Debater</option>
-        <option value="INFJ">INFJ - Advocate</option>
-        <option value="INFP">INFP - Mediator</option>
-        <option value="ENFJ">ENFJ - Protagonist</option>
-        <option value="ENFP">ENFP - Campaigner</option>
-        <option value="ISTJ">ISTJ - Logistician</option>
-        <option value="ISFJ">ISFJ - Defender</option>
-        <option value="ESTJ">ESTJ - Executive</option>
-        <option value="ESFJ">ESFJ - Consul</option>
-        <option value="ISTP">ISTP - Virtuoso</option>
-        <option value="ISFP">ISFP - Adventurer</option>
-        <option value="ESTP">ESTP - Entrepreneur</option>
-        <option value="ESFP">ESFP - Entertainer</option>
-      </select>
-    </div>
-  );
+const mbtiTypes = {
+    INTJ: 'Architect',
+    INTP: 'Logician',
+    ENTJ: 'Commander',
+    ENTP: 'Debater',
+    INFJ: 'Advocate',
+    INFP: 'Mediator',
+    ENFJ: 'Protagonist',
+    ENFP: 'Campaigner',
+    ISTJ: 'Logistician',
+    ISFJ: 'Defender',
+    ESTJ: 'Executive',
+    ESFJ: 'Consul',
+    ISTP: 'Virtuoso',
+    ISFP: 'Adventurer',
+    ESTP: 'Entrepreneur',
+    ESFP: 'Entertainer',
 };
 
-export default MBTI;
+const mbtiGroups = {
+    Analysts: { color: 'rgb(162, 0, 255)', types: ['INTJ', 'INTP', 'ENTJ', 'ENTP'] }, // Purple
+    Diplomats: { color: 'rgb(11, 138, 0)', types: ['INFJ', 'INFP', 'ENFJ', 'ENFP'] }, // Green
+    Sentinels: { color: 'rgb(3, 206, 252)', types: ['ISTJ', 'ISFJ', 'ESTJ', 'ESFJ'] }, // Blue
+    Explorers: { color: 'rgb(252, 186, 3)', types: ['ISTP', 'ISFP', 'ESTP', 'ESFP'] }  // Yellow
+};
+  
+  const MBTI = () => {
+    const [selectedType, setSelectedType] = useState('');
+  
+    const handleSelectType = (typeCode) => {
+      setSelectedType(typeCode);
+    };
+  
+    return (
+        <div className="mbti-container">
+          <h1>Select Your MBTI Personality Type</h1>
+          {Object.entries(mbtiGroups).map(([groupName, groupDetails]) => (
+            <div key={groupName}>
+              <h2 style={{ color: groupDetails.color }}>{groupName}</h2>
+              <div className="mbti-cards">
+                {groupDetails.types.map((typeCode) => (
+                  <div 
+                    key={typeCode} 
+                    className={`mbti-card ${selectedType === typeCode ? 'selected' : ''}`}
+                    onClick={() => handleSelectType(typeCode)}
+                    style={{ backgroundColor: groupDetails.color, borderColor: groupDetails.color }}
+                  >
+                    <h2>{typeCode}</h2>
+                    <p>{mbtiTypes[typeCode]}</p> 
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    };
+    
+    export default MBTI;
