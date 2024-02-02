9ae1510f74ccf81dfa49ddfbdce98147aed17ce4
zwu3 <99296226+zwu3@users.noreply.github.com>
2024-02-01 22:40:45 -0800

    playlist styling

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { getRecommendations } from '../service/spotifyService'; // Import getRecommendations from the appropriate file
-import '../css/Playlist.css'; // Make sure to create and import a CSS file for styling
+import { getRecommendations } from '../service/spotifyService'; // Ensure this is the correct path
+import '../css/Playlist.css'; // Import your CSS file for styling
 
 const Playlist = () => {
   const [tracks, setTracks] = useState([]);
+
+  // Define the color palette
+  const colors = ['FFCB57', 'FB7DA8', 'FD5A46', '552CB7', '00995E', '058CD7'];
+
+  // Function to get color for each track
+  const getColorForTrack = (index) => `#${colors[index % colors.length]}`;
 
   useEffect(() => {
     const fetchRecommendations = async () => {
@@ -26,11 +32,15 @@ const Playlist = () => {
     <div>
       <h1 className="header-title">Recommended Playlist</h1>
       <ul className="playlist">
-        {tracks.map(track => (
-          <li key={track.id} className="track-item">
-            {/* Display track information here, customize as needed */}
-            <span className="track-name">{track.name}</span>
-            {/* You can add more details like artist, album, etc. */}
+        {tracks.map((track, index) => (
+          <li key={track.id} className="track-item" style={{ backgroundColor: getColorForTrack(index) }}>
+            <a href={track.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+              <img src={track.album.images[0].url} alt={track.name} className="album-cover" />
+            </a>
+            <div className="track-info">
+              <span className="track-name">{track.name}</span>
+              <span className="track-artist">{track.artists.map(artist => artist.name).join(', ')}</span>
+            </div>
           </li>
         ))}
       </ul>
